df78a29c6dcee64a29a6a6b05009bfcb862928c3
Ali Shahsavandi <ali.shahsavandi6@gmail.com>
2023-04-09 00:11:21 +0430

    Update fileserver.sh

--- fileserver.sh
+++ fileserver.sh
@@ -6,6 +6,9 @@ docker network create my_network
 # create server volume
 docker volume create servervol
 
+# run mongodb container
+docker run -d --name mongodb --network my_network -v mongodbdata:/data/db mongo
+
 # build and run server container
 docker build -t server .
 docker run -d --name server --mount source=servervol,target=/serverdata --network my_network server
